b7891c172516fa012906c7f3cad6cd43850e349d
刁世浩 <diaoshihao@kanshenmekanbugeinilian.local>
2019-12-26 16:37:40 +0800

    initialize

---
+++ DevelopmentKit-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevelopmentKit-iOS
+//
+//  Created by 刁世浩 on 2019/12/26.
+//  Copyright © 2019 刁世浩. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
